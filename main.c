4573d40b798aea7879c749aeb7404898de2f8618
Youngdo, Lee <ydlee@truen.co.kr>
2021-07-08 17:52:32 +0900

    Refine drawLine
    Signed-off-by: Youngdo, Lee <ydlee@truen.co.kr>

--- main.c
+++ main.c
@@ -8,32 +8,49 @@
 static void drawHLine_BPP8(
 		struct Surface *s, int x, int y, int width, unsigned char color)
 {
+	DBG("HLine (%d, %d, w=%d)\n", x, y, width);
+
 	ASSERT(s);
 	ASSERT(x >= 0);
 	ASSERT(y >= 0);
-	ASSERT(width > 0);
 	ASSERT(x < s->width);
 	ASSERT(y < s->height);
 
-	//DBG("HLine (%d, %d, w=%d)\n", x, y, width);
-	for (int i = 0; i < width && (x + i) < s->width; i++) {
-		setPixel_BPP8(s, x + i, y, color);
+	if (width == 0)
+		return;
+	else if (width > 0) {
+		for (int i = 0; i < width && (x + i) < s->width; i++) {
+			setPixel_BPP8(s, x + i, y, color);
+		}
+	} else {
+		for (int i = 0; i > width && (x + i) >= 0; i--) {
+			setPixel_BPP8(s, x + i, y, color);
+		}
 	}
 }
 
 static void drawVLine_BPP8(
 		struct Surface *s, int x, int y, int height, unsigned char color)
 {
+	DBG("VLine (%d, %d, h=%d)\n", x, y, height);
+
 	ASSERT(s);
 	ASSERT(x >= 0);
 	ASSERT(y >= 0);
-	ASSERT(height > 0);
 	ASSERT(x < s->width);
 	ASSERT(y < s->height);
 
-	//DBG("VLine (%d, %d, h=%d)\n", x, y, height);
-	for (int i = 0; i < height && (y + i) < s->height; i++) {
-		setPixel_BPP8(s, x, y + i, color);
+	if (height == 0)
+		return;
+
+	if (height > 0) {
+		for (int i = 0; i < height && (y + i) < s->height; i++) {
+			setPixel_BPP8(s, x, y + i, color);
+		}
+	} else {
+		for (int i = 0; i > height && (y + i) >= 0; i--) {
+			setPixel_BPP8(s, x, y + i, color);
+		}
 	}
 }
 
@@ -47,9 +64,12 @@ static void drawLine_BPP8(
 		return drawHLine_BPP8(s, MIN(x1, x2), y1, abs(x1 - x2) + 1, color);
 
 	// y = ax + b
-	double a = (double)(y2 - y1) / (double)(x2 - x1);
+	int plus1x = x2 > x1 ? 1 : -1;
+	int plus1y = y2 > y1 ? 1 : -1;
+	double a = (double)(y2 - y1 + plus1y) / (double)(x2 - x1 + plus1x);
 	double b = y1 - a * x1;
 
+	DBG("y = %f * x + %f\n", a, b);
 	if (a > 1.0F || a < -1.0F) {
 		// process horizontally
 		int step = (x2 > x1) ? 1 : -1;
@@ -58,11 +78,14 @@ static void drawLine_BPP8(
 		y0 = y1;
 		for (int x = x1 + step; x != (x2 + step); x += step) {
 			y = (a * x) + b;
-			drawVLine_BPP8(s, x - step, (int)y0, abs((int)y - (int)y0), color);
+			drawVLine_BPP8(s, x - step, (int)y0, (int)y - (int)y0, color);
 			y0 = y;
 		}
 		y = y2;
-		drawVLine_BPP8(s, (int)x2, y0, MAX(abs((int)y - (int)y0), 1), color);
+		if ((int)y >= (int)y0)
+			drawVLine_BPP8(s, (int)x2, y0, (int)y - (int)y0 + 1, color);
+		else
+			drawVLine_BPP8(s, (int)x2, y0, (int)y - (int)y0 - 1, color);
 	} else {
 		// prcess vertically
 		int step = (y2 > y1) ? 1 : -1;
@@ -71,11 +94,14 @@ static void drawLine_BPP8(
 		x0 = x1;
 		for (int y = y1 + step; y != (y2 + step); y += step) {
 			x = (y - b) / a;
-			drawHLine_BPP8(s, (int)x0, y - step, abs((int)x - (int)x0), color);
+			drawHLine_BPP8(s, (int)x0, y - step, (int)x - (int)x0, color);
 			x0 = x;
 		}
 		x = x2;
-		drawHLine_BPP8(s, (int)x0, y2, MAX(abs((int)x - (int)x0), 1), color);
+		if ((int)x >= (int)x0)
+			drawHLine_BPP8(s, (int)x0, y2, (int)x - (int)x0 + 1, color);
+		else
+			drawHLine_BPP8(s, (int)x0, y2, (int)x - (int)x0 - 1, color);
 	}
 }
 
@@ -104,6 +130,39 @@ static void draw(struct Surface *s)
 
 	int sx, sy, dx, dy;
 
+#if	0
+	sx = 100;
+	sy = 0;
+	dx = 102;
+	dy = 1079;
+	drawLine_BPP8(s, sx, sy, dx, dy, 0xf0);
+#endif	/*0*/
+
+#if	0
+	sx = 0;
+	sy = 1079;
+	dx = 2;
+	dy = 0;
+	drawLine_BPP8(s, sx, sy, dx, dy, 0xf0);
+#endif	/*0*/
+
+#if	1
+	sx = 0;
+	sy = 0;
+	dx = 1919;
+	dy = 2;
+	drawLine_BPP8(s, sx, sy, dx, dy, 0xf0);
+#endif	/*0*/
+
+#if	1
+	sx = 1919;
+	sy = 100;
+	dx = 0;
+	dy = 102;
+	drawLine_BPP8(s, sx, sy, dx, dy, 0xf0);
+#endif	/*0*/
+
+#if	0
 	sx = 0;
 	sy = 0;
 	dy = 1079;
@@ -127,6 +186,7 @@ static void draw(struct Surface *s)
 	dy = 0;
 	for (int dx = 0; dx < 1920; dx += 8)
 		drawLine_BPP8(s, sx, sy, dx, dy, 0xf0);
+#endif	/*0*/
 }
 
 static void dumpToFile(struct Surface *s, const char *path)
